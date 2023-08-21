10bae04d9fd9436db7a61bdfc4c42701d580c43f
baldurkm <baldurkm@gmail.com>
2023-08-21 23:51:37 +0000

    Many improvements TD

--- td.js
+++ td.js
@@ -1,6 +1,8 @@
 // Game variables
 var canvas = document.getElementById("gameCanvas");
 var context = canvas.getContext("2d");
+var statusMessage = '';
+var statusMessageTimeout = 0;
 var enemies = [];
 var towers = [];
 var projectiles = [];
@@ -9,6 +11,7 @@ var addTowerMode = false;
 var money = 100;
 var killCount = 0; 
 var spawnInfluence = 0.01; 
+var lastSpawnedOnKillCount = 0;  // Keep track of the kill count on the last spawn of breaker enemy
 
 // Catch click to add tower
 canvas.addEventListener('click', function(event) {
@@ -63,10 +66,123 @@ for(var i = 0; i < gridRows; i++){
 
 // Enemy constructor
 function Enemy(x, y) {
-  this.x = x;
-  this.y = y;
-  this.speed = 2;
-  this.hp = 3;  // Each enemy can withstand 3 hits before getting destroyed
+    this.x = x;
+    this.y = y;
+    this.speed = 2;
+    this.hp = 3;
+    this.direction = 'right';
+	this.justChangedDirection = false;  // New flag to indicate if direction just changed
+
+    this.move = function() {
+        var gridX = Math.floor(this.x / gridSize);
+        var gridY = Math.floor(this.y / gridSize);
+
+        var newPosition = { x: this.x, y: this.y };
+
+        // Calculate new position based on current direction
+        switch (this.direction) {
+            case 'right':
+                newPosition.x += this.speed;
+                break;
+            case 'left':
+                newPosition.x -= this.speed;
+                break;
+            case 'up':
+                newPosition.y -= this.speed;
+                break;
+            case 'down':
+                newPosition.y += this.speed;
+                break;
+        }
+
+        var newGridX = Math.floor(newPosition.x / gridSize);
+        var newGridY = Math.floor(newPosition.y / gridSize);
+
+        // Check if next cell in grid is free
+        if (grid[newGridY] && grid[newGridY][newGridX] === 0) {
+            this.x = newPosition.x;
+            this.y = newPosition.y;
+			this.justChangedDirection = false;  // Reset flag
+        } else if (!this.justChangedDirection) {  // When hitting a tower
+            // If blocked by a tower, pick a random direction other than 'right'
+            var directions = ['left', 'up', 'down'];
+            this.direction = directions[Math.floor(Math.random() * directions.length)];
+			this.justChangedDirection = true;
+        }
+    }
+}
+
+//Breaker Enemy Constructor
+function BreakerEnemy(x, y) {
+    this.x = x;
+    this.y = y;
+    this.speed = 1; // Breaker enemy moves slower
+    this.hp = 5; // More HP because this enemy is stronger
+    this.direction = 'right';
+    this.justChangedDirection = false;
+
+    this.move = function() {
+        var gridX = Math.floor(this.x / gridSize);
+        var gridY = Math.floor(this.y / gridSize);
+
+        var newPosition = { x: this.x, y: this.y };
+
+        // Calculate new position based on current direction
+        switch (this.direction) {
+            case 'right':
+                newPosition.x += this.speed;
+                break;
+            case 'left':
+                newPosition.x -= this.speed;
+                break;
+            case 'up':
+                newPosition.y -= this.speed;
+                break;
+            case 'down':
+                newPosition.y += this.speed;
+                break;
+        }
+
+        var newGridX = Math.floor(newPosition.x / gridSize);
+        var newGridY = Math.floor(newPosition.y / gridSize);
+
+        // Check if next cell in grid is free
+        if (grid[newGridY] && grid[newGridY][newGridX] === 0) {
+            this.x = newPosition.x;
+            this.y = newPosition.y;
+			this.justChangedDirection = false;  // Reset flag
+        } else if (!this.justChangedDirection) {  // When hitting a tower
+            // If blocked by a tower, pick a random direction other than 'right'
+            var directions = ['left', 'up', 'down'];
+            this.direction = directions[Math.floor(Math.random() * directions.length)];
+			this.justChangedDirection = true;
+        }
+    };
+    this.breakTower = function(){
+    // Convert the position of the enemy from pixels to grid
+    var gridX = Math.floor(this.x / gridSize);
+    var gridY = Math.floor(this.y / gridSize);
+
+    // Check nearby cells in a 3x3 area
+    for(var dy=-1; dy<=1; dy++){
+        for(var dx=-1; dx<=1; dx++){
+            // Check if this cell is inside the grid's boundary
+            if(gridY+dy >= 0 && gridY+dy < gridRows && gridX+dx >= 0 && gridX+dx < gridColumns){
+                // If there's a tower in this cell, remove it
+                if(grid[gridY+dy][gridX+dx] === 1){
+                    grid[gridY+dy][gridX+dx] = 0;
+                    // Loop through all towers, find the one standing in this grid cell and remove it
+                    for(var i = 0; i < towers.length; i++){
+                        if(towers[i].x === (gridX+dx) * gridSize && towers[i].y === (gridY+dy) * gridSize){
+                            towers.splice(i, 1);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
 }
 
 // Projectile constructor
@@ -124,11 +240,14 @@ function placeTower(x, y) {
 
     // Boundary condition fix
     if (i >= gridRows || j >= gridColumns) {
-      alert("Invalid tower location!");
+statusMessage = 'Invalid tower location';
+statusMessageTimeout = 120;  // Show message for 120 frames (4 seconds at 30 FPS)
     } else if (grid[i][j] == 1) {
-      alert("Cannot place a tower on top of another!");
+statusMessage = 'Cant build a tower on another tower';
+statusMessageTimeout = 120;  
     } else if (money < 50) {
-      alert("Insufficient money to place a tower!");
+statusMessage = 'Insufficient funds';
+statusMessageTimeout = 120;  
     } else {
       towers.push(new Tower(j * gridSize, i * gridSize));
       grid[i][j] = 1;
@@ -165,17 +284,27 @@ context.fillText("BUILD $50", addTowerButton.x + 10, addTowerButton.y + 20);
 
     
     // Move and Draw enemies
-    for(var i in enemies) {
-        var enemy = enemies[i];
-        enemy.x += enemy.speed;
-        
-        context.fillStyle = enemy.hp === 3 ? "red" : enemy.hp === 2 ? "orange" : "yellow";
-        context.fillRect(enemy.x, enemy.y, 30, 30);
+for(var i in enemies) {
+    var enemy = enemies[i];
+
+enemy.move();
+if (enemy instanceof BreakerEnemy) {
+    enemy.breakTower();
+}
+
+if (enemy instanceof BreakerEnemy) {
+    context.fillStyle = enemy.hp === 5 ? "Purple" : enemy.hp > 3 ? "orange" : "yellow";
+} else {
+    context.fillStyle = enemy.hp === 3 ? "red" : enemy.hp === 2 ? "orange" : "yellow";
+}
+
+    context.fillRect(enemy.x, enemy.y, gridSize, gridSize);
 		
 		 // Check for lose condition
-		if (enemy.x >= canvas.width) {
+		if (enemy.x + gridSize >= canvas.width) {
+				statusMessage = 'The enemies have reached our base. Abandon all hope.';
+				statusMessageTimeout = 120;  
 				clearInterval(gameLoop);  // End the game loop
-				alert('Game Over! An enemy reached your base.');
 				return;  // If you want to stop execution after losing
 		}
 	
@@ -186,6 +315,15 @@ context.fillText("BUILD $50", addTowerButton.x + 10, addTowerButton.y + 20);
 	context.font = "16px Arial";
 	context.fillText("CASH: " + money, 10, 30);
 	context.fillText("KILLS: " + killCount, 10, 60);
+// Draw messages	
+context.fillStyle = 'red';
+context.fillText(statusMessage, canvas.width/2, canvas.height/2);
+if(statusMessageTimeout > 0) {
+    statusMessageTimeout--;
+} else {
+    statusMessage = '';
+}
+
 
     // Tower firing and drawing
     for (var i in towers) {
@@ -206,23 +344,24 @@ context.fillText("BUILD $50", addTowerButton.x + 10, addTowerButton.y + 20);
         var velocityY = (dy / distance) * projectile.speed;
 		projectile.life--;
 
-        if(distance < 1){
-            // Decrease enemy's HP
-            enemy.hp--;
+		if(distance < 1 || enemy.hp <= 0){
+			// Decrease enemy's HP
+			enemy.hp--;
 
-            // If enemy's HP reached zero, delete it
+			// If enemy's HP reached zero, delete it
 			if (enemy.hp <= 0){
 				var enemyIndex = enemies.indexOf(enemy);
 				if (enemyIndex > -1){
 					enemies.splice(enemyIndex, 1);
+					// Award for killing an enemy
+					money += 20;
+					killCount++;  // Increase kill count when enemy is destroyed
 				}
-				// Award for killing an enemy
-				money += 20;
-				killCount++;  // Increase kill count when enemy is destroyed
 			}
-            // Remove the projectile
-            projectiles.splice(i, 1);
-        } else {
+			// Remove the projectile
+			projectiles.splice(i, 1);
+			break; // This break will prevent other projectiles from erroneously registering a hit on the same enemy in this loop iteration
+		   } else {
             // Move the projectile
             projectile.x += velocityX;
             projectile.y += velocityY;
@@ -235,9 +374,11 @@ context.fillText("BUILD $50", addTowerButton.x + 10, addTowerButton.y + 20);
 			projectiles.splice(i, 1);
 		}		
     }
-	
+
+
+
 		// Spawn rate
-	if (killCount >= 300) {
+	/*if (killCount >= 300) {
         spawnInfluence = 0.05;
 	} else if (killCount >= 100) {
         spawnInfluence = 0.04;
@@ -245,10 +386,16 @@ context.fillText("BUILD $50", addTowerButton.x + 10, addTowerButton.y + 20);
         spawnInfluence = 0.03;
     } else if (killCount >= 20) {
         spawnInfluence = 0.02;
-    }
+    }*/
+	// Exponential Spawn rate
+spawnInfluence = 0.01 * Math.exp(killCount / 20.0);
 
-    // Enemy spawning
-    if(Math.random() < spawnInfluence) {
-        enemies.push(new Enemy(0, Math.random() * (canvas.height - 30)));
-    }
+
+if(killCount % 10 === 0 && killCount > 0 && lastSpawnedOnKillCount != killCount){
+   enemies.push(new BreakerEnemy(0, Math.random() * (canvas.height - 30)));
+   lastSpawnedOnKillCount = killCount;
+} else if(Math.random() < spawnInfluence) {
+    enemies.push(new Enemy(0, Math.random() * (canvas.height - 30)));
+}
+
 }, 30);
