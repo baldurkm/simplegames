b7c6fc055b755c54c150b4799f42914204110237
baldurkm <baldurkm@gmail.com>
2024-01-10 09:43:18 +0000

    Update mapPage.js

--- trail/mapPage.js
+++ trail/mapPage.js
@@ -2,6 +2,17 @@ function displayMapPage()
 {
     console.log("Showing Map Page");
 
+        function createDirectionButtons() {
+        const directions = ['Up', 'Down', 'Left', 'Right'];
+    
+        directions.forEach(direction => {
+            const button = createButton(direction, `${direction.toLowerCase()}-button`);
+            button.addEventListener('click', function() {
+                followPath(direction.toLowerCase());
+            });
+        });
+    }
+
         // Hide inbox
         const inboxContainer = document.getElementById('inbox-container');
         inboxContainer.style.display = 'none';
